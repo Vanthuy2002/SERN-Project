9a130188ac6dbc4dc69dc70909fc9f6fee627fef
Thuy Nguyen <vanthuy101102@gmail.com>
2023-09-08 23:16:43 +0700

    refactor code -> ORM

--- backend/src/Services/user.services.js
+++ backend/src/Services/user.services.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const { AsyncUsersDb, UsersDb } = require('../configDB/Users');
+const db = require('../models');
 
 const hashUserPassword = (password) => {
   const salt = bcrypt.genSaltSync(10);
@@ -7,48 +7,32 @@ const hashUserPassword = (password) => {
   return hashPass;
 };
 
-const createUser = (email, password, username) => {
+const createUser = async (email, password, username) => {
   if (!email || !password) {
     throw new Error('Please enter complete data');
   }
   const hashPass = hashUserPassword(password);
-
-  UsersDb.query(
-    `INSERT INTO users (email, password, username) VALUES (?, ?, ?)`,
-    [email, hashPass, username],
-    (err, data) => {
-      err && console.log(err.toString());
-      console.log(data);
-    }
-  );
+  await db.User.create({ email, password: hashPass, username });
 };
 
 const getUsers = async () => {
-  const connection = await AsyncUsersDb;
-  const [rows] = await connection.execute('SELECT * FROM `users`');
-  return rows;
+  const users = await db.User.findAll();
+  return users;
 };
 
 const deleteUser = async (id) => {
-  const connection = await AsyncUsersDb;
-  await connection.execute('DELETE FROM users WHERE id=?', [id]);
+  await db.User.destroy({
+    where: { id },
+  });
 };
 
 const getUserById = async (id) => {
-  const connection = await AsyncUsersDb;
-  const [rows] = await connection.execute('SELECT * FROM users WHERE id=?', [
-    id,
-  ]);
-  return rows[0];
+  const user = await db.User.findOne({ where: { id } });
+  return user;
 };
 
 const updateUser = async (username, email, id) => {
-  const connection = await AsyncUsersDb;
-  const [data] = await connection.execute(
-    'UPDATE users SET email = ?, username = ? WHERE id = ?',
-    [email, username, id]
-  );
-  return data;
+  await db.User.update({ username, email }, { where: { id } });
 };
 
 module.exports = { createUser, getUsers, deleteUser, getUserById, updateUser };
